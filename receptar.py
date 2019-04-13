ab827955b49bc5edf8683e2307a15604061e9834
Tereza Klášterecká <tereza.klasterecka@gmail.com>
2019-04-13 17:20:11 +0200

    Program se zeptá uživatele na ingredienci a vypíše názvy receptů, které ji obsahují

--- receptar.py
+++ receptar.py
@@ -13,9 +13,34 @@ def nacti_recept(jmeno):
     with open(cesta_k_receptu, 'r', encoding='utf-8') as f:
         return f.read()
 
+def projdi_recept(ingredience,jmeno):
+    """
+    Když této funkci dáš ingredienci, projde recepty, jestli v nich někde je
+    """
+    tenhle_script = __file__
+    adresar_projektu = os.path.dirname(tenhle_script)
+    adresar_receptu = os.path.join(adresar_projektu, 'recepty')
+    cesta_k_receptu = os.path.join(adresar_receptu, jmeno)
+
+    ingredience = ingredience.lower()
+    with open(cesta_k_receptu, 'r', encoding='utf-8') as f:
+        text = f.read()
+        text = text.lower()
+        if ingredience in text:
+            print("V receptu {} je ingredience {}".format(jmeno,ingredience))
+            return True
+        else:
+            return False
+
 print(nacti_recept('titulek.txt'))
 
 NAZVY_RECEPTU = os.listdir('recepty')
 
 for recept in NAZVY_RECEPTU:
-    print(nacti_recept(recept))
+    if "titulek.txt" not in recept:
+        print(nacti_recept(recept))
+
+ingredience = input("Zadej nazev ingredience: ")
+
+for recept in NAZVY_RECEPTU:
+    projdi_recept(ingredience,recept)
